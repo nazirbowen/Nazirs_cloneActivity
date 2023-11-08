66da22bf652ef3238415ba5cba48dacfa126bc12
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-11-08 01:00:25 +0000

    done

---
+++ Nazirs_gitCloneFunction.py
@@ -0,0 +1,9 @@
+def profileData():
+    Goal = input('what is your ideal weight goal?')
+    name = input('whats your name?')
+    picture = input('upload a before picture')
+    CurrentWeight= int(input('What is your current weight'))
+    WeightCounter = float(input('How much would you like to lose or gain'))
+    print('Welcome to your weight journey.')
+
+profileData()
